261e0ef7456cdf96db3b9e01d41044c5ebd1422e
EC2 Default User <ec2-user@ip-172-31-16-145.ec2.internal>
2019-01-02 16:10:06 +0000

    transfer to new ide

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181229015757) do
+ActiveRecord::Schema.define(version: 20181229020813) do
 
   create_table "businesses", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -123,7 +123,7 @@ ActiveRecord::Schema.define(version: 20181229015757) do
     t.integer  "rewardcost"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-    t.string   "Rewarddescription"
+    t.string   "rewarddescription"
     t.index ["reward_id"], name: "index_rewardpurchases_on_reward_id"
     t.index ["user_id"], name: "index_rewardpurchases_on_user_id"
   end
